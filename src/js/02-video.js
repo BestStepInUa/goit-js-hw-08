4dadb321819ad473f9944a45f97c2f0cd6797f8b
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-08 14:37:50 +0300

    all_done_v1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// time update tracking function with throttling
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// onPlay callback function for save player time to local storage
+function onPlay({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+// restart video from the time saved to local storage
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+// for autoplay just add src like into iframe html tag
+// src = "https://player.vimeo.com/video/236203659?autoplay=1&loop=1&autopause=0&muted=1"
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+// throttle lib
+import throttle from 'lodash.throttle';
+// DOM El refs
+const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea')
+}
 
+// init obj for storing inputs data
+let formData = {};
+
+populateForm();
+
+// form input func
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+// form submit func
+refs.form.addEventListener('submit', onFormSubmit);
+
+// form input handler
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+// form submit handler
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    formData = {};
+}
+// func of reading data from Local Storege and their writing into forms fields
+function populateForm() {
+    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedData) {
+        const { email, message } = savedData;
+        if (email) {            
+            refs.input.value = email;
+            formData.email = email;     
+        
+        }
+        if (message) {
+            refs.textarea.value = message;
+            formData.message = message;  
+        }
+    }    
+}
